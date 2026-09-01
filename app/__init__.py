a887fad0d1791985890555058e429782bbc77410
ZagrosGM <310271424+ZagrosGM@users.noreply.github.com>
2026-09-01 19:14:20 +0000

    chore(release): move the version line to 1.0.0-beta.5

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ import asyncio
 import logging
 import time
 
-__version__ = "1.0.0-beta.4"  # Zagros begins a new version line after the rebrand
+__version__ = "1.0.0-beta.5"  # Zagros begins a new version line after the rebrand
 
 
 _building = False
